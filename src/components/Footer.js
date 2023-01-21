4a7827bb5951b803d7b2633ed0dc511f9ba7a1cf
Aleksa Djordjevic <aleksa.djordjevic2312@gmail.com>
2023-01-21 17:16:24 +0100

    Dodata kuka za broj putovanja

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 const Footer = () => {
+    const [count, setCount] = useState(0);
+
+
     return (
         <footer>
         <p>Copyright &copy; 2023</p>
         <p>Aleksa Djordjevic</p>
         <Link to='/about'>About</Link>
+        <br/>
+        <p>Number of trips I planned: {count}</p>
+        <button onClick={() => setCount(count + 1)}>
+        Add trip to count
+        </button>
         </footer>
     )
 }
